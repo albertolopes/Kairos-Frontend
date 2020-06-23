58614f1577e9fc0608a08396752c37846ef0c923
albertolopes <alberto_llopes@hotmail.com>
2020-06-23 16:03:11 -0300

    Login/SignUp

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <Todo/>
+  </v-app>
+</template>
+
+<script>
+
+//import Login from './components/Auth/Login'
+// import Signup from './components/Auth/Signup'
+import Todo from './components/Todo'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-form>
+                    <v-card class="elevation-12">
+                        <v-toolbar dark color="blue">
+                            <v-toolbar-title>
+                                Login
+                            </v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-text-field
+                                name="login"
+                                label="Login"
+                                type="text"
+                            ></v-text-field>   
+                            <v-text-field
+                                name="password"
+                                label="Password"
+                                type="password"
+                            ></v-text-field>    
+                        </v-card-text>
+                        <v-divider light></v-divider>
+                        <v-card-actions>
+                            <v-btn round color="indigo" dark>Sign in</v-btn>
+                            <v-spacer></v-spacer>
+                            <v-btn round color="primary" dark>
+                                Login
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-form>    
+            </v-flex>
+        </v-layout>
+    </v-container>    
+</template>
+
+<script>
+export default {
+    name: 'login'
+}; 
+</script>
